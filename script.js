2818361c97dd1d6dce7c6c18c8875e45c5a0c317
sasa <pufie.study@gmail.com>
2025-06-22 12:28:50 -0300

    +

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu(icon) {
     const menu = document.getElementById("menu");
     menu.classList.toggle("show"); // aparece/some o menu
 }
+
+
+// perfil lateral
+const painelPerfil = document.getElementById('painel-perfil');
+const btnFecharPerfil = document.getElementById('fecharPerfil');
+const btnAbrirPerfil = document.getElementById('meu-perfil'); // o link/ícone do usuário com esse id
+
+btnAbrirPerfil.addEventListener('click', e => {
+  e.preventDefault();
+  painelPerfil.classList.add('aberto');
+});
+
+btnFecharPerfil.addEventListener('click', () => {
+  painelPerfil.classList.remove('aberto');
+});
